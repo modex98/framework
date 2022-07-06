e099a035c581869b64311aa22c280b21897b8212
Daniel Roe <daniel@roe.dev>
2022-07-06 15:00:35 +0100

    feat(nuxt): tree-shake templates

---
+++ test/fixtures/basic/pages/client.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    This page should not crash when rendered.
+    <ClientOnly class="something">
+      test
+      <BreaksServer />
+      <BreaksServer>Some slot content</BreaksServer>
+    </ClientOnly>
+    This should render.
+    <div>
+      <ClientOnly class="another">
+        test
+        <BreaksServer />
+      </ClientOnly>
+    </div>
+  </div>
+</template>
